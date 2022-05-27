8ca7ba157cd0c59c259bb9d30e8beb54ff5d0ea2
chenchenshao <csc522684186@163.com>
2022-05-27 20:20:25 +0800

    完成商品详情页面

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,19 +1,176 @@
 <template>
-  <view>
-    <h2>ggg</h2>
+  <!-- //数据请求回来才显示页面 -->
+  <view v-if="goods_info.goods_name" class="goods-detail-container">
+    <!-- //商品详情轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">
+        {{goods_info.goods_price}}
+      </view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <!-- 商品名称 -->
+        <view class="goods-name">
+          {{goods_info.goods_name}}
+        </view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <view class="yf">
+        快递：免运费
+      </view>
+    </view>
+
+
+    <!-- 商品导航组件 -->
+    <view class="goods-nav">
+      <!-- fill配置按钮是否平铺 -->
+      <!-- options配置左侧按钮，buttonGroup配置右侧按钮 -->
+      <!-- click配置左侧按钮点击事件，buttonClick配置右侧按钮点击事件 -->
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick"></uni-goods-nav>
+    </view>
+    <!-- 商品详情 -->
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
   </view>
 </template>
 
 <script>
   export default {
+    onLoad(options) {
+      const goods_id = options.goods_id
+      this.getGoodsDetail(goods_id)
+    },
     data() {
       return {
-        
+        // 商品详情对象
+        goods_info: {},
+        // 左侧按钮配置对象
+        options: [{
+          icon: 'shop',
+          text: '店铺'
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        // 右侧按钮配置对象
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
       };
+    },
+    methods: {
+      //获取商品详情数据
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail', {
+          goods_id
+        })
+        if (res.meta.status !== 200) return uni.$showMsg('数据获取失败!')
+        // 使用字符串的 replace() 方法，为 img 标签添加行内的 style 样式，从而解决图片底部空白间隙的问题  正则替换。再将图片格式替换为jpg
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+          .replace(/webp/g, 'jpg')
+        this.goods_info = res.message
+      },
+      // 点击轮播图图片进行图片预览
+      preview(i) {
+        uni.previewImage({
+          // 预览时,默认显示图片的索引
+          current: i,
+          //利用数组的映射方法获得所有图片的url地址数组
+          urls: this.goods_info.pics.map(x => x.pics_big)
+        })
+      },
+      // 商品导航栏左侧按钮点击事件
+      onClick(e){
+        if(e.content.text==='购物车'){
+         uni.switchTab({
+           url:'/pages/cart/cart'
+         })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .goods-detail-container{
+    // 防止拉到最底部商品导航栏覆盖部分内容
+    padding-bottom: 50px;
+  }
+  
+  //轮播图样式
+  swiper {
+    height: 750rpx;
 
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  //商品信息样式
+  .goods-info-box {
+    padding: 10px 0 10px 0;
+    
+    .price {
+      color: #c00000;
+      font-size: 18px;
+      margin: 10px 0;
+    }
+
+    .goods-info-body {
+      display: flex;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+        padding: 10px;
+      }
+
+      // 收藏
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #efefef;
+        color: gray;
+      }
+    }
+
+    //运费
+    .yf {
+      margin: 10px 0;
+      font-size: 12px;
+      color: gray;
+    }
+  }
+  
+  .goods-nav{
+    position: fixed;  
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
